dabe44f8165fa4504a21687b2a114dd4ac633ddb
winston-bosan <winston-bosan@gmail.com>
2018-09-28 14:32:34 -0400

    Database mod

--- src/models/index.js
+++ src/models/index.js
@@ -2,18 +2,35 @@ import Sequelize from "sequelize";
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-  });
+console.log(process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, process.env.RDS_HOSTNAME, process.env.RDS_PORT)
+
+if (process.env.REMOTE) {
+  console.log('We are doing this remotely')
+  sequelize = new Sequelize(
+    process.env.RDS_DB_NAME,
+    process.env.RDS_USERNAME,
+    process.env.RDS_PASSWORD,
+    {
+      host: process.env.RDS_HOSTNAME,
+      port: process.env.RDS_PORT,
+      maxConcurrentQueries: 100,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: "Amazon RDS"
+      },
+      pool: { maxConnections: 1, maxIdleTime: 30 },
+      language: "en"
+    }
+  );
 } else {
+  console.log('We are doing this locally')
   sequelize = new Sequelize(
     process.env.TEST_DATABASE || process.env.DATABASE,
     process.env.DATABASE_USER,
     process.env.DATABASE_PASSWORD,
     {
-      dialect: 'postgres',
-    },
+      dialect: "postgres"
+    }
   );
 }
 
